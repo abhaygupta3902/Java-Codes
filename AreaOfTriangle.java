ee89dff75c70bffbf97cc8b070f6eafdee9e4811
abhaygupta3 <abhaygupta030902@gmail.com>
2023-02-20 12:33:01 +0530

    code converted into OOPs format

--- AreaOfTriangle.java
+++ AreaOfTriangle.java
@@ -2,15 +2,33 @@ import java.util.Scanner;
 
 class AreaOfTriangle 
 {
+	int base,height;
+	float area=0.0f;
+	
+	AreaOfTriangle(int b, int h)
+	{
+		base = b;	
+		height = h;
+	}
+	void calArea()
+	{
+		area=(base*height)/2;	
+	}
+	float getArea()
+	{
+		return area;
+	}
 	public static void main(String[] args)
 	{
-		int base,height;
-		float area=0.0f;
+		
+		int b,h;
 		System.out.println("Enter the values of base and height :");
 		Scanner sc = new Scanner(System.in);
-		base = sc.nextInt();
-		height = sc.nextInt();
-		area=(base*height)/2;
-		System.out.println("the area of a triangle is "+area);
+		b = sc.nextInt();
+		h = sc.nextInt();
+		
+		AreaOfTriangle obj = new AreaOfTriangle(b,h);
+		obj.calArea();
+		System.out.println("the area of a triangle is "+obj.getArea());
 	}
 }
